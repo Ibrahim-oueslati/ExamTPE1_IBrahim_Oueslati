21fcef7e63c424a7a5b5e9e88bbabd042c46f0a9
Ibrahim <ibrahim.oueslati1@outlook.com>
2025-07-03 18:46:52 +0100

    question 15

--- projet.py
+++ projet.py
@@ -9,3 +9,11 @@ Data=[1,3,5]
 Som=sum(Data)
 print('la somme est:',som)
 
+if Data :
+    print('la somme est :',sum(Data))
+    print('la min est :',min(Data))
+    print('le max est :',max(Data))
+else :
+    print('dossier vide')
+
+
